00b32e2cdcbc66d58eb4ab5c029666f80162c9cb
Ryan Provost <rprovost@new-host-2.home>
2015-01-15 22:42:10 -0500

    #2 Fixed indentation and updates Karma config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,81 @@
 module.exports = function(grunt) {
-
-
-    grunt.initConfig({
-        jshint: {
-            files: ['src/**/*.js'],
-            options: {
-                globals: {
-                    angular: true
-                }
-            }
-        },
-        uglify: {
-            source: {
-                options: {
-                    sourceMap: true,
-                    sourceMapIncludeSources: true,
-                    compress: {
-                        drop_console: true
-                    }
-                },
-                files: {
-                    'dist/ng-backstretch.min.js': ['src/ng-backstretch.js']
-                }
-            }
-        },
-        copy: {
-            source: {
-                files: [
-                    {'dist/ng-backstretch.js': ['src/ng-backstretch.js']}
-                ]
-            }
-        },
-        karma: {
-            unit: {
-                options: {
-                    background: true,
-                    singleRun: false,
-                    browsers: ['phantomJS'],
-                    plugins: ['karma-jasmine', 'karma:coverage', 'karma-phantomjs-launcher'],
-                    files: [
-                        'bower_components/angular/angular.js',
-                        'src/**/*.js'
-                    ],
-                }
-            }
-        },
-        watch: {
-            files: ['src/**/*.js'],
-            tasks: ['jshint', 'karma:unit:run']
+  grunt.initConfig({
+    jshint: {
+      files: ['src/**/*.js'],
+      options: {
+        globals: {
+          angular: true
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-karma');
+    uglify: {
+      source: {
+        options: {
+          sourceMap: true,
+          sourceMapIncludeSources: true,
+          compress: {
+            drop_console: true
+          }
+        },
+        files: {
+          'dist/ng-backstretch.min.js': ['src/ng-backstretch.js']
+        }
+      }
+    },
 
-    grunt.registerTask('build', ['jshint', 'uglify:source', 'copy:source']);
+    copy: {
+      source: {
+        files: [
+          {'dist/ng-backstretch.js': ['src/ng-backstretch.js']}
+        ]
+      }
+    },
+
+    karma: {
+      unit: {
+        autoWatch: true,
+        singleRun: false,
+        options: {
+          frameworks: ['jasmine'],
+          browsers: ['PhantomJS'],
+          plugins: ['karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher'],
+          files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'src/**/*.js'
+          ]
+        }
+      },
+
+      single_run: {
+        singleRun: true,
+        options: {
+          frameworks: ['jasmine'],
+          browsers: ['PhantomJS'],
+          plugins: ['karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher'],
+          reporters: ['coverage','progress'],
+          coverageReporter: {type: 'text'},
+          files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'src/**/*.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      files: ['src/**/*.js'],
+      tasks: ['jshint', 'karma:unit:run']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('build', ['jshint', 'uglify:source', 'copy:source']);
 };
